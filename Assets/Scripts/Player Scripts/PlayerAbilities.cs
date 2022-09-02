32f1c94dd140830aadcab0f04da81f34ee140dd8
AyoJunji <joseramos@knights.ucf.edu>
2022-09-01 21:57:00 -0400

    Cinemachine Camera setup and starting several other scripts

---
+++ Assets/Scripts/Player Scripts/PlayerAbilities.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAbilities : MonoBehaviour
+{
+    [Header("Purchased Items")]
+    public bool boughtDash;
+    public bool boughtNuke;
+    public bool boughtScytheThrow;
+
+    [Header("Abilities")]
+    public int currentNukes;
+    public int maxNukes;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/StoreManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreManager : MonoBehaviour
+{
+    [Header("Player Inventory")]
+    private int soulsAmount;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
